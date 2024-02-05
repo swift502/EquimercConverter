5f2b6afcf9da0d472d4ef8656e22fb53612ef5f2
Jan Bláha <blaha.j502@gmail.com>
2024-02-05 12:33:55 +0100

    shader

--- modgl.py
+++ modgl.py
@@ -10,7 +10,8 @@ vertex_shader = """
 #version 330
 in vec2 in_vert;
 out vec2 fragCoord;
-void main() {
+void main()
+{
     gl_Position = vec4(in_vert, 0.0, 1.0);
     fragCoord = (in_vert + 1) / 2;
 }
@@ -22,14 +23,71 @@ fragment_shader = """
 in vec2 fragCoord;
 out vec4 fragColor;
 uniform sampler2D texture;
-void main() {
-    fragColor = texture2D(texture, fragCoord);
+
+#define M_PI 3.1415926535897932384626433832795
+#define M_E 2.7182818284590452353602874713527
+//#define M_MMAX 1.48442223358
+
+float remap(float value, float oldMin, float oldMax, float newMin, float newMax)
+{
+    return ((value - oldMin) / (oldMax - oldMin)) * (newMax - newMin) + newMin;
+}
+
+vec2 equi_to_merc(float u, float v)
+{
+    // uv to equirectangular
+    float lat = remap(u, 0, 1, 0, 2 * M_PI);
+    float lon = remap(v, 0, 1, -M_PI * 0.5, M_PI * 0.5);
+
+    // equirectangular to mercator
+    float x = lat;
+    //lon = clamp(lon, -M_MMAX, M_MMAX);
+    float y = log(tan(M_PI / 4 + lon / 2));
+
+    // mercator to uv
+    x = remap(x, 0, 2 * M_PI, 0, 1);
+    y = remap(y, -M_PI, M_PI, 0, 1);
+
+    // clamp
+    x = clamp(x, 0, 1);
+    y = clamp(y, 0, 1);
+
+    return vec2(x, y);
+}
+
+vec2 merc_to_equi(float u, float v)
+{
+    // uv to mercator
+    float lat = remap(u, 0, 1, 0, 2 * M_PI);
+    float lon = remap(v, 0, 1, -M_PI, M_PI);
+
+    // mercator to equirectangular
+    float x = lat;
+    float y = 2 * atan(pow(M_E, lon)) - M_PI * 0.5;
+
+    // equirectangular to uv
+    x = remap(x, 0, 2 * M_PI, 0, 1);
+    y = remap(y, -M_PI * 0.5, M_PI * 0.5, 0, 1);
+
+    // clamp
+    x = clamp(x, 0, 1);
+    y = clamp(y, 0, 1);
+
+    return vec2(x, y);
+}
+
+void main()
+{
+    vec2 uv = merc_to_equi(fragCoord.x, fragCoord.y);
+    //vec2 uv = equi_to_merc(fragCoord.x, fragCoord.y);
+    fragColor = texture2D(texture, uv);
 }
 """
 
 # Load the input image
 input_image_path = "./data/equi.png"
 input_image = Image.open(input_image_path)
+input_image = input_image.resize((input_image.width, input_image.height * 2), Image.Resampling.NEAREST)
 
 # Create a shader program
 prog = ctx.program(vertex_shader=vertex_shader, fragment_shader=fragment_shader)
@@ -68,6 +126,6 @@ pixels = fbo.read(components=channels, alignment=1)
 ctx.release()
 
 # Save the result as a PNG file
-output_image = Image.frombytes('RGBA', (width, height), pixels)
+output_image = Image.frombytes(mode, (width, height), pixels)
 # output_image = output_image.transpose(Image.FLIP_TOP_BOTTOM)
 output_image.save("output_image_rendered.png")
