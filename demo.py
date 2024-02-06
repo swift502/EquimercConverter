0e382610bdaa39b8d015f2e9f324a529c6ad11ce
Jan Bláha <blaha.j502@gmail.com>
2024-02-06 17:26:43 +0100

    paths

--- demo.py
+++ demo.py
@@ -5,27 +5,27 @@ from src.enums import CONVERSION, DEVICE, SAMPLING
 
 # Convert to mercator
 Converter.convert(
-    input="./img/equi.png",
-    output="./output/equi_to_merc_GPU_LINEAR.png",
+    input="img/equi.png",
+    output="output/equi_to_merc_GPU_LINEAR.png",
     conversion=CONVERSION.TO_MERCATOR)
 
 # Nearest sampling
 Converter.convert(
-    input="./img/equi.png",
-    output="./output/equi_to_merc_GPU_NEAREST.png",
+    input="img/equi.png",
+    output="output/equi_to_merc_GPU_NEAREST.png",
     conversion=CONVERSION.TO_MERCATOR,
     sampling=SAMPLING.NEAREST)
 
 # Convert to equirectangular
 Converter.convert(
-    input="./img/merc.jpg",
-    output="./output/merc_to_equi_GPU_LINEAR.jpg",
+    input="img/merc.jpg",
+    output="output/merc_to_equi_GPU_LINEAR.jpg",
     conversion=CONVERSION.TO_EQUIRECTANGULAR)
 
 # Nearest sampling
 Converter.convert(
-    input="./img/merc.jpg",
-    output="./output/merc_to_equi_GPU_NEAREST.jpg",
+    input="img/merc.jpg",
+    output="output/merc_to_equi_GPU_NEAREST.jpg",
     conversion=CONVERSION.TO_EQUIRECTANGULAR,
     sampling=SAMPLING.NEAREST)
 
@@ -33,14 +33,14 @@ Converter.convert(
 
 # Convert to mercator
 Converter.convert(
-    input="./img/equi.png",
-    output="./output/equi_to_merc_CPU.png",
+    input="img/equi.png",
+    output="output/equi_to_merc_CPU.png",
     conversion=CONVERSION.TO_MERCATOR,
     device=DEVICE.CPU)
 
 # Convert to equirectangular
 Converter.convert(
-    input="./img/merc.jpg",
-    output="./output/merc_to_equi_CPU.jpg",
+    input="img/merc.jpg",
+    output="output/merc_to_equi_CPU.jpg",
     conversion=CONVERSION.TO_EQUIRECTANGULAR,
     device=DEVICE.CPU)
